47e8e6739e59842d4a5fcaf2a071804e3199d47f
Moreira <im.senne2017@gmail.com>
2021-02-11 11:43:07 +0000

    CheckBox changed Color

--- app/src/main/java/com/senne/implementations/MainActivity.kt
+++ app/src/main/java/com/senne/implementations/MainActivity.kt
@@ -2,15 +2,24 @@ package com.senne.implementations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.CheckBox
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val teste : TextView = findViewById(R.id.teste)
-        teste.hint = "Testandoooo"
-        val teste2 : TextView = findViewById(R.id.teste)
-        teste2.text = "Novo Text"
+
+
+        val checkBox : CheckBox = findViewById(R.id.checkBox)
+        val scissor : ImageView = findViewById(R.id.scissor)
+        checkBox.setOnClickListener{
+            if(checkBox.isChecked) scissor.setColorFilter(resources.getColor( R.color.red))
+            if(!checkBox.isChecked) scissor.setColorFilter(resources.getColor( R.color.black))
+        }
+
+
     }
+
+
 }
